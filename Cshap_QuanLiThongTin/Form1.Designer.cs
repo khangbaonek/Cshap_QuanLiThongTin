2194f01eb28226bd3825a84f16b052cd1714f1f5
Administrator <Administrator@LAB1-MAY06>
2024-06-24 16:17:26 +0700

    Add project files.

---
+++ Cshap_QuanLiThongTin/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Cshap_QuanLiThongTin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maTen = new System.Windows.Forms.TextBox();
+            this.hoTen = new System.Windows.Forms.TextBox();
+            this.namsinh = new System.Windows.Forms.TextBox();
+            this.quenquan = new System.Windows.Forms.TextBox();
+            this.tentinkiem = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(58, 216);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(749, 222);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mã Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Họ Tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Năm Sinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Quê Quán";
+            // 
+            // maTen
+            // 
+            this.maTen.Location = new System.Drawing.Point(168, 51);
+            this.maTen.Name = "maTen";
+            this.maTen.Size = new System.Drawing.Size(247, 20);
+            this.maTen.TabIndex = 4;
+            // 
+            // hoTen
+            // 
+            this.hoTen.Location = new System.Drawing.Point(168, 76);
+            this.hoTen.Name = "hoTen";
+            this.hoTen.Size = new System.Drawing.Size(247, 20);
+            this.hoTen.TabIndex = 4;
+            // 
+            // namsinh
+            // 
+            this.namsinh.Location = new System.Drawing.Point(168, 110);
+            this.namsinh.Name = "namsinh";
+            this.namsinh.Size = new System.Drawing.Size(247, 20);
+            this.namsinh.TabIndex = 4;
+            this.namsinh.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // quenquan
+            // 
+            this.quenquan.Location = new System.Drawing.Point(168, 141);
+            this.quenquan.Name = "quenquan";
+            this.quenquan.Size = new System.Drawing.Size(247, 20);
+            this.quenquan.TabIndex = 4;
+            // 
+            // tentinkiem
+            // 
+            this.tentinkiem.Location = new System.Drawing.Point(539, 190);
+            this.tentinkiem.Name = "tentinkiem";
+            this.tentinkiem.Size = new System.Drawing.Size(247, 20);
+            this.tentinkiem.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(890, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(890, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(890, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 52);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Thêm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(890, 231);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 52);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Sủa";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(890, 289);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 52);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Xóa";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(890, 347);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(129, 52);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Tìm Kiếm";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(890, 405);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(129, 52);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Trở về";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 558);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tentinkiem);
+            this.Controls.Add(this.quenquan);
+            this.Controls.Add(this.namsinh);
+            this.Controls.Add(this.hoTen);
+            this.Controls.Add(this.maTen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox maTen;
+        private System.Windows.Forms.TextBox hoTen;
+        private System.Windows.Forms.TextBox namsinh;
+        private System.Windows.Forms.TextBox quenquan;
+        private System.Windows.Forms.TextBox tentinkiem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Cshap_QuanLiThongTin/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Cshap_QuanLiThongTin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=LAB1-MAY06\MISASME2022;Initial Catalog=QuanLyThongTin;Integrated Security=True");
+        private void openCon()
+        {
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        private void closeCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+        private Boolean Exe(string cmd)
+        {
+            openCon();
+            Boolean check;
+            try
+            {
+                SqlCommand sc = new SqlCommand(cmd, con);
+                sc.ExecuteNonQuery();
+                check = true;
+            }
+            catch (Exception)
+            {
+                check = false;
+                throw;
+            }
+            closeCon();
+            return check;
+        }
+        private DataTable Red(string cmd)
+        {
+            openCon();
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand sc = new SqlCommand(cmd, con);
+                SqlDataAdapter sda = new SqlDataAdapter(sc);
+                sda.Fill(dt);
+            }
+            catch (Exception)
+            {
+                dt = null;
+                throw;
+            }
+            closeCon();
+            return dt;
+        }
+        private void load()
+        {
+            DataTable dt = Red("SELECT * FROM QuanLyThongTin");
+            if(dt != null)
+            {
+                dataGridView1.DataSource = dt;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            maTen.ResetText();
+            hoTen.ResetText();
+            namsinh.ResetText();
+            quenquan.ResetText();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Exe("INSERI INTO quanlythongtin() VALUSE")
+        }
+    }
+}
